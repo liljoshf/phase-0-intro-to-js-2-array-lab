1ce25d32782e58b412578c97eae517470bb31921
Josh <liljoshf@yahoo.com>
2021-08-14 18:36:10 -0400

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,89 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(name) {
+cats.push("Ralph");
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyPrependCat(name) {
+cats.unshift("Bob");
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyRemoveLastCat(name) {
+cats.pop();
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function appendCat(name) { 
+const catArrayCopy = [...cats]
+catArrayCopy.push(name)
+return catArrayCopy
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function prependCat(name) {
+const catArrayCopy = [...cats]
+catArrayCopy.unshift(name)
+return catArrayCopy
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function removeLastCat(name) {
+const catArrayCopy = [...cats]
+catArrayCopy.pop()
+return catArrayCopy
+}
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function removeFirstCat(name) {
+const catArrayCopy = [...cats]
+catArrayCopy.shift()
+return catArrayCopy
+}
